6bf7a0456ac934e499eff45c7cb8e717693267ec
MananPP <96185095+MananPP@users.noreply.github.com>
2025-04-03 17:40:21 +0530

    3

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
+};
+
+export default nextConfig;
